61dbb63acc3e0bb66ccb2caf0c73f2fa2fff6ded
Tar Law <tarlaw@gmail.com>
2015-07-03 15:54:02 +0800

    Modify init local function

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,13 +1,12 @@
-(add-to-list 'load-path (expand-file-name "lisp/local" user-emacs-directory))
-
 (require-package 'evil-leader)
-(after-load 'evil-leader
+(require-package 'evil)
+
+(when (maybe-require-package 'evil-leader)
   (global-evil-leader-mode)
   (evil-leader/set-key "sh" 'shell)
   (evil-leader/set-key "x" 'execute-extended-command))
 
-(require-package 'evil)
-(after-load 'evil
+(when (maybe-require-package 'evil)
   (evil-mode 1))
 
 (provide 'init-local)
