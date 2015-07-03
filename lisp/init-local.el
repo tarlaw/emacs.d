f6dcd57be1c1493d9da791996818c324beb6c2f1
Tar Law <tarlaw@gmail.com>
2015-07-03 11:22:58 +0800

    Add automatic download evil

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,13 +1,13 @@
 (add-to-list 'load-path (expand-file-name "lisp/local" user-emacs-directory))
 
-(require 'evil-leader)
-(global-evil-leader-mode)
+(require-package 'evil-leader)
+(after-load 'evil-leader
+  (global-evil-leader-mode)
+  (evil-leader/set-key "sh" 'shell)
+  (evil-leader/set-key "x" 'execute-extended-command))
 
-(evil-leader/set-key "sh" 'shell)
-(evil-leader/set-key "x" 'execute-extended-command)
-
-(require 'evil)
-(evil-mode 1)
-
+(require-package 'evil)
+(after-load 'evil
+  (evil-mode 1))
 
 (provide 'init-local)
