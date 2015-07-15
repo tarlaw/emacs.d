61fde748e98c74a280c1499351d84342988db2f5
Tar Law <tarlaw@gmail.com>
2015-07-15 09:10:33 +0800

    Add server-edit key

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -4,7 +4,10 @@
 (when (maybe-require-package 'evil-leader)
   (global-evil-leader-mode)
   (evil-leader/set-key "sh" 'shell)
-  (evil-leader/set-key "x" 'execute-extended-command))
+  (evil-leader/set-key "x" 'execute-extended-command)
+  (evil-leader/set-key "cc" 'org-babel-execute-src-block)
+  (evil-leader/set-key "co" 'org-babel-open-src-block-result)
+  (evil-leader/set-key "bd" 'server-edit))
 
 (when (maybe-require-package 'evil)
   (evil-mode 1))
