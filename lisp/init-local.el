2a56a0ad6bdfc443d361474b8bb557760bf89c86
Tar Law <tarlaw@gmail.com>
2015-08-27 17:12:10 +0800

    Add chinese font support

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -12,4 +12,76 @@
 (when (maybe-require-package 'evil)
   (evil-mode 1))
 
+(org-babel-do-load-languages
+  'org-babel-load-languages
+  '((emacs-lisp . t)
+    (python . t)
+    (sh . t)
+    (ditaa . t)
+    (plantuml . t)
+    (latex . t)
+    (ruby . t)
+    (R . t)))
+
+;; Setting English Font
+;(set-face-attribute
+;  'default nil :font "Consolas 12")
+
+;; Chinese Font
+;(dolist (charset '(kana han symbol cjk-misc bopomofo))
+;    (set-fontset-font (frame-parameter nil 'font)
+;                      charset
+;                      (font-spec :family "Microsoft Yahei" :size 18)))
+
+;;
+
+(defun qiang-font-existsp (font)
+  (if (null (x-list-fonts font))
+      nil
+    t))
+
+(defvar font-list '("Microsoft Yahei" "文泉驿等宽微米黑" "黑体" "新宋体" "宋体"))
+(require 'cl) ;; find-if is in common list package
+(find-if #'qiang-font-existsp font-list)
+
+(defun qiang-make-font-string (font-name font-size)
+  (if (and (stringp font-size)
+           (equal ":" (string (elt font-size 0))))
+      (format "%s%s" font-name font-size)
+    (format "%s %s" font-name font-size)))
+
+(defun qiang-set-font (english-fonts
+                       english-font-size
+                       chinese-fonts
+                       &optional chinese-font-size)
+
+  "english-font-size could be set to \":pixelsize=18\" or a integer.
+If set/leave chinese-font-size to nil, it will follow english-font-size"
+  (require 'cl) ; for find if
+  (let ((en-font (qiang-make-font-string
+                  (find-if #'qiang-font-existsp english-fonts)
+                  english-font-size))
+        (zh-font (font-spec :family (find-if #'qiang-font-existsp chinese-fonts)
+                            :size chinese-font-size)))
+
+    ;; Set the default English font
+    ;;
+    ;; The following 2 method cannot make the font settig work in new frames.
+    ;; (set-default-font "Consolas:pixelsize=18")
+    ;; (add-to-list 'default-frame-alist '(font . "Consolas:pixelsize=18"))
+    ;; We have to use set-face-attribute
+    (message "Set English Font to %s" en-font)
+    (set-face-attribute 'default nil :font en-font)
+
+    ;; Set Chinese font
+    ;; Do not use 'unicode charset, it will cause the English font setting invalid
+    (message "Set Chinese Font to %s" zh-font)
+    (dolist (charset '(kana han symbol cjk-misc bopomofo))
+      (set-fontset-font (frame-parameter nil 'font)
+                        charset zh-font))))
+(qiang-set-font
+ '("Consolas" "Monaco" "DejaVu Sans Mono" "Monospace" "Courier New") ":pixelsize=12"
+ '("Microsoft Yahei" "Heiti SC" "文泉驿等宽微米黑" "黑体" "新宋体" "宋体")
+ 14)
+
 (provide 'init-local)
